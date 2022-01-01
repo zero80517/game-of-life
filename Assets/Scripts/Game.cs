d25950482639b3be1c9a271209bed64cd63f7fa9
zero80517 <zero80517@gmail.com>
2022-01-01 19:13:31 +0300

    Working game of life
    Added population control, that set cells alive or dead.

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -7,6 +7,10 @@ public class Game : MonoBehaviour
     private static int SCREEN_WIDTH = 64;   //units = 1024 pixels / 16 pixels
     private static int SCREEN_HEIGHT = 48;  //units = 768 pixels / 16 pixels
 
+    public float speed = 0.1f;              //seconds for 1 frame 
+
+    private float timer = 0;
+
     Cell[,] grid = new Cell[SCREEN_WIDTH, SCREEN_HEIGHT];
 
     // Start is called before the first frame update
@@ -18,6 +22,18 @@ public class Game : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (timer >= speed)
+        {
+            timer = 0f;
+
+            CountNeighbors();
+
+            PopulationControl();
+        }
+        else
+        {
+            timer += Time.deltaTime;
+        }
         
     }
 
@@ -38,6 +54,126 @@ public class Game : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Count number of alive neighbors and save in grid.numNeighbors
+    /// </summary>
+    void CountNeighbors()
+    {
+        for (int y = 0; y < SCREEN_HEIGHT; y++)
+        {
+            for (int x = 0; x < SCREEN_WIDTH; x++)
+            {
+                int numNeighbors = 0;
+
+                // North
+                if (y + 1 < SCREEN_HEIGHT)
+                {
+                    if (grid[x, y + 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+                // East
+                if (x + 1 < SCREEN_WIDTH)
+                {
+                    if (grid[x + 1, y].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+                // South
+                if (y - 1 >= 0)
+                {
+                    if (grid[x, y - 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+                // West
+                if (x - 1 >= 0)
+                {
+                    if (grid[x - 1, y].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+                // NorthEast
+                if (x + 1 < SCREEN_WIDTH && y + 1 < SCREEN_HEIGHT)
+                {
+                    if (grid[x + 1, y + 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+                // NorthWest
+                if (x - 1 >= 0 && y + 1 < SCREEN_HEIGHT)
+                {
+                    if (grid[x - 1, y + 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+                // SouthEast
+                if (x + 1 < SCREEN_WIDTH && y - 1 >= 0)
+                {
+                    if (grid[x + 1, y - 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+                // SouthWest
+                if (x - 1 >= 0 && y - 1 >= 0)
+                {
+                    if (grid[x - 1, y - 1].isAlive)
+                    {
+                        numNeighbors++;
+                    }
+                }
+
+                grid[x, y].numNeighbors = numNeighbors;
+            }
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void PopulationControl()
+    {
+        for (int y = 0; y < SCREEN_HEIGHT; y++)
+        {
+            for (int x = 0; x < SCREEN_WIDTH; x++)
+            {
+                // Rules:
+                // - Any live cell with 2 or 3 live neighbors survives;
+                // - Any dead cell with 3 live neighbors becomes a live cell;
+                // - All other live cells die in the next generation
+                // and all other dead cells stay dead.
+                if (grid[x, y].isAlive)
+                {
+                    // Cell is Alive
+
+                    if (grid[x, y].numNeighbors != 2 && grid[x, y].numNeighbors != 3)
+                    {
+                        grid[x, y].SetAlive(false);
+                    }
+                }
+                else
+                {
+                    // Cell is Dead
+                    if (grid[x, y].numNeighbors == 3)
+                    {
+                        grid[x, y].SetAlive(true);
+                    }
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Create random true or false
+    /// </summary>
+    /// <returns>true or false</returns>
     bool RandomAliveCell()
     {
         int rand = UnityEngine.Random.Range(0, 100);
